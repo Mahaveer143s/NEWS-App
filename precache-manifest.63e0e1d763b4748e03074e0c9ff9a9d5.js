e7422f438046ba12f1252d9b212da67a3881ae4e
Mahaveer Suthar <110727771+Mahaveer143s@users.noreply.github.com>
2024-06-22 09:25:17 +0530

    Updates

---
+++ precache-manifest.63e0e1d763b4748e03074e0c9ff9a9d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "699f9c12b6ee19fd5b60733b84ca2a14",
+    "url": "/NEWS-App/index.html"
+  },
+  {
+    "revision": "571f504e9f836dc1bdb1",
+    "url": "/NEWS-App/static/css/main.0d3e0ea3.chunk.css"
+  },
+  {
+    "revision": "914e5ea7fd5a8eca84f8",
+    "url": "/NEWS-App/static/js/2.e3fa4815.chunk.js"
+  },
+  {
+    "revision": "43b07b59a8799f3362738c8366935dda",
+    "url": "/NEWS-App/static/js/2.e3fa4815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fb729eddda31342b02",
+    "url": "/NEWS-App/static/js/3.027a8587.chunk.js"
+  },
+  {
+    "revision": "571f504e9f836dc1bdb1",
+    "url": "/NEWS-App/static/js/main.02a95583.chunk.js"
+  },
+  {
+    "revision": "a3a11c05cb8989051080",
+    "url": "/NEWS-App/static/js/runtime-main.b2ce3ecd.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/NEWS-App/precache-manifest.63e0e1d763b4748e03074e0c9ff9a9d5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NEWS-App/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
